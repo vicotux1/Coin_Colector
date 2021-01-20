67e94ea6ec0cb21532f8bc5fc9e3f64350ebbef4
DarioMirandaYT <66534526+vicotux1@users.noreply.github.com>
2021-01-19 23:03:05 -0300

    Player-update
    New 3d models Player (tenis Ball && Soccer Ball)

---
+++ Assets/RotationTranslation/RotationTranslationAnimation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationTranslationAnimation : MonoBehaviour
+{
+
+    /*ESPAÑOL
+     *Solución por GameDevTraum
+    * 
+    * Artículo: https://gamedevtraum.com/es/descargar-assets/scripts-para-unity/script-de-rotacion-y-traslacion-para-gameobjects-en-unity/
+    * Página: https://gamedevtraum.com/es/
+    * Canal: https://youtube.com/c/GameDevTraum
+    * 
+    * Visita la página para encontrar más soluciones, Assets y artículos
+   */
+
+
+    [SerializeField][Range(1f, 15f)]float rotationX;
+    [SerializeField][Range(1f, 15f)]float rotationY;
+    [SerializeField][Range(1f, 15f)]float rotationZ;
+    
+
+    void FixedUpdate(){
+
+        Rotation();       
+    }
+
+    private void Rotation(){
+         transform.Rotate(rotationX, rotationY, rotationZ, Space.Self);
+        }
+
+}
