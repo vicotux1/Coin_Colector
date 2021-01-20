d8878cabd6533b62d7a9713c66218c692e6b354d
DarioMirandaYT <66534526+vicotux1@users.noreply.github.com>
2021-01-19 23:12:51 -0300

    Rotation update
    -Remove coins animations
    -Add Script to rotate Coins using Physics (FixedUpdate)

---
+++ Assets/Coin_colector/scripts/RotationTranslationAnimation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationTranslationAnimation : MonoBehaviour{
+    [SerializeField][Range(1f, 15f)]float rotationX;
+    [SerializeField][Range(1f, 15f)]float rotationY;
+    [SerializeField][Range(1f, 15f)]float rotationZ;
+    
+
+    void FixedUpdate(){ Rotation();}
+        void Rotation(){
+         transform.Rotate(rotationX, rotationY, rotationZ, Space.Self);}
+
+}
